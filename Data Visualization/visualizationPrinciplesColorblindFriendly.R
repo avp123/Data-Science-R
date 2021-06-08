1633e63968edf64b0674d3df6426855f7131d7ff
avp123 <woffly123@gmail.com>
2021-06-07 19:23:30 -0700

    Simple scatterplot demonstrating how to make seeing graphs less difficult for colorblind viewers.

---
+++ Data Visualization/visualizationPrinciplesColorblindFriendly.R
@@ -0,0 +1,10 @@
+# Create vector with colorblind friendly colors
+color_blind_friendly_cols <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+# Create dataframe with ggplot
+p1 <- data.frame(x = 1:8, y = 1:8, col = as.character(1:8)) %>%
+  ggplot(aes(x, y, color = col)) +
+  geom_point(size = 5)
+
+# Add colorblind friendly vector to color scheme
+p1 + scale_color_manual(values = color_blind_friendly_cols)
