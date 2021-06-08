29c748047c023bb77141be01aa5a5a51e5fcefa9
avp123 <woffly123@gmail.com>
2021-06-07 19:06:01 -0700

    Scatter plots made on heights data set, using principles of jitter and alpha blending to make graph more interpretable.

---
+++ Data Visualization/visualizationPrinciplesJitter&Alpha.R
@@ -0,0 +1,9 @@
+# load and inspect heights data
+library(dslabs)
+data(heights)
+
+# dot plot showing the data
+heights %>% ggplot(aes(sex, height)) + geom_point()
+
+# jittered, alpha blended point plot
+heights %>% ggplot(aes(sex, height)) + geom_jitter(width = 0.1, alpha = 0.2)
