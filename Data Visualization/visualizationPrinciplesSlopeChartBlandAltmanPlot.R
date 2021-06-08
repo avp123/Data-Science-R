2c70d338d9d4e756a74fc55a590b76c09d9d2a2c
avp123 <woffly123@gmail.com>
2021-06-08 10:06:35 -0700

    Analysis of gapminder data using Slope Charts and Bland-Altman plots

---
+++ Data Visualization/visualizationPrinciplesSlopeChartBlandAltmanPlot.R
@@ -0,0 +1,40 @@
+# Load in libraries
+library(tidyverse)
+library(dslabs)
+library(ggrepel)
+data(gapminder)
+
+
+#
+# Create country vector
+west <- c("Western Europe", "Northern Europe", "Southern Europe", "Northern America", "Australia and New Zealand")
+
+# Filter gapminder for year, region, and population
+dat <- gapminder %>%
+  filter(year %in% c(2010, 2015) & region %in% west & !is.na(life_expectancy) & population > 10^7)
+
+# Slope chart showing change in life expectancy from 2010 to 2015
+dat %>%
+  mutate(location = ifelse(year == 2010, 1, 2),
+         location = ifelse(year == 2015 & country %in% c("United Kingdom", "Portugal"),
+                           location + 0.22, location),
+         hjust = ifelse(year == 2010, 1, 0)) %>%
+  mutate(year = as.factor(year)) %>%
+  ggplot(aes(year, life_expectancy, group = country)) +
+  geom_line(aes(color = country), show.legend = FALSE) +
+  geom_text(aes(x = location, label = country, hjust = hjust), show.legend = FALSE) +
+  xlab("") +
+  ylab("Life Expectancy")
+
+# Bland-Altman plot showing average life expectancy between years (x-axis) and change in life expectancy between years (y-axis)
+dat %>%
+  mutate(year = paste0("life_expectancy_", year)) %>%
+  select(country, year, life_expectancy) %>% spread(year, life_expectancy) %>%
+  mutate(average = (life_expectancy_2015 + life_expectancy_2010)/2,
+         difference = life_expectancy_2015 - life_expectancy_2010) %>%
+  ggplot(aes(average, difference, label = country)) +
+  geom_point() +
+  geom_text_repel() +
+  geom_abline(lty = 2) +
+  xlab("Average of 2010 and 2015") +
+  ylab("Difference between 2015 and 2010")
