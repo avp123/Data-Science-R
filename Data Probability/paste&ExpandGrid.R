6847c9711c415891748de8f83886ffffa7757b4d
avp123 <woffly123@gmail.com>
2021-06-14 14:16:18 -0700

    Used paste() and expand.grid() to create a deck of cards and found the probability of drawing a king from the deck

---
+++ Data Probability/paste&ExpandGrid.R
@@ -0,0 +1,21 @@
+# paste() and expand.grid()
+# joining strings with paste
+number <- "Three"
+suit <- "Hearts"
+paste(number, suit)
+
+# joining vectors element-wise with paste
+paste(letters[1:5], as.character(1:5))
+
+# generating combinations of 2 vectors with expand.grid
+expand.grid(pants = c("blue", "black"), shirt = c("white", "grey", "plaid"))
+
+# Generating deck of cards
+suits <- c("Diamonds", "Clubs", "Hearts", "Spades")
+numbers <- c("Ace", "Deuce", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King")
+deck <- expand.grid(number = numbers, suit = suits)
+deck <- paste(deck$number, deck$suit)
+
+# probability of drawing a king
+kings <- paste("King", suits)
+mean(deck %in% kings)
