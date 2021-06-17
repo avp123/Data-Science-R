dc2319345a31b9c5d666fed54857e44bf5e206ec
avp123 <woffly123@gmail.com>
2021-06-17 13:51:39 -0700

    Added more probabilities that show how alcohol usage and tobacco usage affect the probability that a person is a cancer case or control (doesn't have cancer).

--- Data Probability/esophagealCancerDatasetProbabilities.R
+++ Data Probability/esophagealCancerDatasetProbabilities.R
@@ -42,3 +42,91 @@ tob_controls <- esoph %>%
 
 tob_controls/all_controls
 
+# Probability that a person who is a cancer case is in the highest alcohol usage group
+high_alc_cases <- esoph %>%
+  filter(alcgp == "120+") %>%
+  pull(ncases) %>%
+  sum()
+
+p_case_high_alc <- high_alc_cases/all_cases
+p_case_high_alc
+
+# Probability that a person who is a cancer case is in the highest tobacco usage group
+high_tob_cases <- esoph %>%
+  filter(tobgp == "30+") %>%
+  pull(ncases) %>%
+  sum()
+
+p_case_high_tob <- high_tob_cases/all_cases
+p_case_high_tob
+
+# Probability that a person who is a cancer case is in the highest tobacco usage group AND the highest alcohol usage group
+high_alc_and_tob_cases <- esoph %>%
+  filter(alcgp == "120+" & tobgp == "30+") %>%
+  pull(ncases) %>%
+  sum()
+
+p_case_high_alc_and_tob <- high_alc_and_tob_cases/all_cases
+p_case_high_alc_and_tob
+
+# Probability that a person who is a cancer case is in the highest tobacco usage group OR the highest alcohol usage group
+high_alc_or_tob_cases <- esoph %>%
+  filter(alcgp == "120+" | tobgp == "30+") %>%
+  pull(ncases) %>%
+  sum()
+
+p_case_high_alc_or_tob <- high_alc_or_tob_cases/all_cases
+p_case_high_alc_or_tob
+
+# OR use addition rule
+p_case_either_highest <- p_case_high_alc + p_case_high_tob - p_case_high_alc_and_tob
+p_case_either_highest
+
+# Probability control is in highest alcohol group
+high_alc_controls <- esoph %>%
+  filter(alcgp == "120+") %>%
+  pull(ncontrols) %>%
+  sum()
+
+p_control_high_alc <- high_alc_controls/all_controls
+p_control_high_alc
+
+# Difference in likelihood of cases being in highest alcohol group compared to controls
+p_case_high_alc / p_control_high_alc
+
+# Probability control is in highest tobacco group
+high_tob_controls <- esoph %>%
+  filter(tobgp == "30+") %>%
+  pull(ncontrols) %>%
+  sum()
+
+p_control_high_tob <- high_tob_controls/all_controls
+p_control_high_tob
+
+# Probability that a person who is a control is in the highest tobacco usage group AND the highest alcohol usage group
+high_alc_and_tob_controls <- esoph %>%
+  filter(alcgp == "120+" & tobgp == "30+") %>%
+  pull(ncontrols) %>%
+  sum()
+
+p_control_high_alc_and_tob <- high_alc_and_tob_controls/all_controls
+p_control_high_alc_and_tob
+
+# Probability that a person who is a control is in the highest tobacco usage group OR the highest alcohol usage group
+high_alc_or_tob_controls <- esoph %>%
+  filter(alcgp == "120+" | tobgp == "30+") %>%
+  pull(ncontrols) %>%
+  sum()
+
+p_control_high_alc_or_tob <- high_alc_or_tob_controls/all_controls
+p_control_high_alc_or_tob
+
+# OR use addition rule
+p_control_either_highest <- p_control_high_alc + p_control_high_tob - p_control_high_alc_and_tob
+p_control_either_highest
+
+# Difference in likelihood of cases being in highest alcohol or tobacco group compared to controls in the same groups
+p_case_high_alc_or_tob / p_control_high_alc_or_tob
+
+# Or using addition rule variables
+p_case_either_highest / p_control_either_highest
