f6be95b772071512881c9e07aecf716cb3dc667b
avp123 <woffly123@gmail.com>
2021-06-23 10:51:11 -0700

    Added probabilities of other events within the ACT data set and used quantiles and quantile plots to create a visualization of one of these probabilities.

--- Data Probability/ACTScores.R
+++ Data Probability/ACTScores.R
@@ -23,3 +23,33 @@ f_x <- dnorm(x, 20.9, 5.7)
 data.frame(x, f_x) %>%
   ggplot(aes(x, f_x)) +
   geom_line()
+
+# Creating Z scores from act_scores and finding probability that z_score is greater than 2 standard deviation above the mean
+z_scores <- (act_scores - mean(act_scores))/sd(act_scores)
+mean(z_scores > 2)
+
+# Finding Act score 2 standard deviations above the mean
+2*sd(act_scores) + mean(act_scores)
+
+# Finding the 97.5th percentile of act_scores using qnorm()
+qnorm(.975, mean(act_scores), sd(act_scores))
+
+# Finding minimum integer score so that probability of the score is at least .95
+cdf <- sapply(1:36, function (x){
+  mean(act_scores <= x)
+})
+min(which(cdf >= .95))
+
+# Expected 95th percentile of ACT scores given mean of 20.9 and sd of 5.7
+qnorm(.95, 20.9, 5.7)
+
+# Using quantiles to determine the percentile the score of 26 is in
+p <- seq(0.01, 0.99, 0.01)
+sample_quantiles <- quantile(act_scores, p)
+names(sample_quantiles[max(which(sample_quantiles < 26))])
+
+# QQ plot showing theoretical_quantiles on the x axis and sample_quantiles on the y axis with line of best fit
+p <- seq(0.01, 0.99, 0.01)
+sample_quantiles <- quantile(act_scores, p)
+theoretical_quantiles <- qnorm(p, 20.9, 5.7)
+qplot(theoretical_quantiles, sample_quantiles) + geom_abline()
