cb890b00f502c9c738b82fcb8919e8bf1074a309
avp123 <woffly123@gmail.com>
2021-07-23 13:09:25 -0700

    Modeled confidence intervals using geom_smooth in a graph of temperature data. Then ran a monte carlo and did calculations to determine confidence intervals and calculate them for random numbers

---
+++ Inference and Modeling/confidenceIntervalsCalculations.R
@@ -0,0 +1,21 @@
+# Example of confidence interval with temperatures
+data("nhtemp")
+data.frame(year = as.numeric(time(nhtemp)), temperature = as.numeric(nhtemp)) %>%
+  ggplot(aes(year, temperature)) +
+  geom_point() +
+  geom_smooth() +
+  ggtitle("Average Yearly Temperatures in New Haven")
+
+# Monte Carlo of confidence intervals
+p <- 0.45
+N <- 1000
+X <- sample(c(0,1), size = N, replace = TRUE, prob = c(1-p, p))    # generate N observations
+X_hat <- mean(X)    # calculate X_hat
+SE_hat <- sqrt(X_hat*(1-X_hat)/N)    # calculate SE_hat, SE of the mean of N observations
+c(X_hat - 2*SE_hat, X_hat + 2*SE_hat)    # build interval of 2*SE above and below mean
+
+# Finding z using qnorm
+z <- qnorm(0.995)    # calculate z to solve for 99% confidence interval
+pnorm(qnorm(0.995))    # demonstrating that qnorm gives the z value for a given probability
+pnorm(qnorm(1-0.995))    # demonstrating symmetry of 1-qnorm
+pnorm(z) - pnorm(-z)    # demonstrating that this z value gives correct probability for interval
